ffcccbd5caf7035a9fca4db763a60d9604598564
DGchangbin <129083264+DGchangbin@users.noreply.github.com>
2023-04-14 17:04:42 +0900

    Create 9498.py

---
+++ 임창빈/4.14/9498.py
@@ -0,0 +1,11 @@
+test = int(input())
+if 90<=test<=100:
+    print("A")
+elif 80<=test<=89:
+    print("B")
+elif 70<=test<=79:
+    print("C")
+elif 60<=test<=69:
+    print("D")
+else:
+    print("F")
