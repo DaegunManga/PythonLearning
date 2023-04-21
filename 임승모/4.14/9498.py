42fc7ea3277b93f8e3b61e3bb1c04444cbe840a9
ImSeungMo <128972511+ImSeungMo@users.noreply.github.com>
2023-04-21 15:53:35 +0900

    Create 9498.py

---
+++ 임승모/4.14/9498.py
@@ -0,0 +1,11 @@
+result = int(input())
+if result <= 100 and result >= 90:
+    print("A")
+elif result <=89  and result >= 80:
+    print("B")
+elif result <=79  and result >= 70:
+    print("C")
+elif result <=69  and result >= 60:
+    print("D")
+else:
+    print("F")
