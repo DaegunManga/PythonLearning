916ffb94af12d72a73ab4671a2219d800c7b54f0
tmdgks1310 <129189838+tmdgks1310@users.noreply.github.com>
2023-04-21 15:44:45 +0900

    Create 9498.py

---
+++ 박승한/4.14/9498.py
@@ -0,0 +1,15 @@
+A = input()
+A = int (A)
+if A>=90 and A<= 100:
+    print("A")
+
+if A>=80 and A< 90: 
+    print("B")
+
+if A>=70 and A< 80:
+    print("C")
+
+if A>=60 and A< 70:
+    print("D")
+if A < 60:
+    print("F")
